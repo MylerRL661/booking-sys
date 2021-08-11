7599be10006a6e1898ba706256781a033eecfbbc
unknown <mylesleslie2@gmail.com>
2021-08-11 22:33:47 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -11,15 +11,18 @@ function App() {
   <li><a href="Check In Page.html">Check In</a></li>
   <li class="right" ><a href="Login.html">Login</a></li>
 </ul>
-<h1>Wren Booking System</h1>
+<h1>Device Wrentals</h1>
 <br></br>
 <p>
   Please select the device needed from the drop down list below:
 </p>
 <label for="Devices">Choose your device:</label>
 <select name="Devices" id="Devices">
-  <option value="iPhone">iPhone</option>
-  <option value="Laptop 1">Laptop 1</option>
+  <option value="Phone Android">Phone (Android)</option>
+  <option value="Phone Apple">Phone (Apple)</option>
+  <option value="Tablet Apple">Tablet (Apple)</option>
+  <option value="Tablet Android">Tablet (Android)</option>
+  <option value="Laptop">Laptops</option>
 </select>
 </body>
 </div>
